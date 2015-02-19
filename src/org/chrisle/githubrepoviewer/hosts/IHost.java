dfd36f147d818d77eba9fc181f16218394c979a5
Chris <chrl@spreadshirt.net>
2015-02-19 16:23:52 +0100

    Added interface for host provider.

---
+++ src/org/chrisle/githubrepoviewer/hosts/IHost.java
@@ -0,0 +1,14 @@
+package org.chrisle.githubrepoviewer.hosts;
+
+import java.util.List;
+
+/**
+ *
+ * @author chrl
+ */
+public interface IHost {
+    public List<String> getRepositories();
+    public List<String> getBranches();
+    public List<String> getTags();
+    public Boolean isPrivate();
+}
