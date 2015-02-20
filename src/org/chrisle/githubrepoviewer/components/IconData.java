3cafe47b49c86cb2d2048aef3df8412697ef712a
Chris <chrl@spreadshirt.net>
2015-02-20 14:16:19 +0100

    Created helper classes to set icon to the tree

---
+++ src/org/chrisle/githubrepoviewer/components/IconData.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.chrisle.githubrepoviewer.components;
+
+import javax.swing.Icon;
+
+/**
+ *
+ * @author chrl
+ */
+class IconData {
+
+    protected Icon _icon;
+    protected Icon _openIcon;
+    protected Object _data;
+
+    public IconData(Icon icon, Object data) {
+        _icon = icon;
+        _openIcon = null;
+        _data = data;
+    }
+
+    public IconData(Icon icon, Icon openIcon, Object data) {
+        _icon = icon;
+        _openIcon = openIcon;
+        _data = data;
+    }
+
+    public Icon getIcon() {
+        return _icon;
+    }
+
+    public Icon getOpenIcon() {
+        return _openIcon != null ? _openIcon : _icon;
+    }
+
+    public Object getObject() {
+        return _data;
+    }
+
+    public String toString() {
+        return _data.toString();
+    }
+}
