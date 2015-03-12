51c92cf9a3103fd6f6b879ddbd2ec0e532a798f4
Chris <chrl@spreadshirt.net>
2015-03-12 07:59:49 +0100

    Added AddHostAction.

---
+++ Repository viewer/src/org/chrisle/gitrepoviewer/service/AddHostAction.java
@@ -0,0 +1,26 @@
+package org.chrisle.gitrepoviewer.service;
+
+/**
+ *
+ * @author chrl
+ */
+import org.openide.util.NbBundle.Messages;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import org.chrisle.gitrepoviewer.components.AddHostDialog;
+
+public class AddHostAction extends AbstractAction {
+    AddHostDialog _addHostDialog;
+
+    @Messages("LBL_AddHost=Add Host")
+    public AddHostAction() {
+        super(Bundle.LBL_AddHost());
+        
+        _addHostDialog = new AddHostDialog(null, true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        _addHostDialog.setVisible(true);
+    }
+}
