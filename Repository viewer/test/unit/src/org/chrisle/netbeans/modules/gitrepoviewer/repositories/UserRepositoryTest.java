297e8acda8d7d842f56835b0f68cfff503f6a608
Chris <chrl@spreadshirt.net>
2015-12-22 15:04:32 +0100

    Fixed tests to test getUser method.

--- Repository viewer/test/unit/src/org/chrisle/netbeans/modules/gitrepoviewer/repositories/UserRepositoryTest.java
+++ Repository viewer/test/unit/src/org/chrisle/netbeans/modules/gitrepoviewer/repositories/UserRepositoryTest.java
@@ -1,15 +1,19 @@
 package org.chrisle.netbeans.modules.gitrepoviewer.repositories;
 
 import com.google.gson.Gson;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import org.chrisle.netbeans.modules.gitrepoviewer.beans.User;
 import org.junit.After;
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 /**
@@ -17,48 +21,60 @@ import org.powermock.modules.junit4.PowerMockRunner;
  * @author chrl
  */
 @RunWith(PowerMockRunner.class)
-//@PrepareForTest({
-//    UserRepository.class
-//})
+@PrepareForTest({
+//    FileReader.class,
+    Gson.class
+})
+//public class UserRepositoryTest extends PowerMockTestCase {
 public class UserRepositoryTest {
     private UserRepository _userRepo;
+    private final Gson _gsonProvider = PowerMockito.mock(Gson.class);
 
-//    @Mock
-    private Gson _gsonProvider;
-
-//    @Mock
+    @Mock
     private FileReader _fileReader;
-    private String _host;
+    private String _selectedHost;
+    private String _dirName;
+    private String _filePrefix;
 
     @Before
     public void setUp() {
-        this._host = "Github";
-        this._gsonProvider = PowerMockito.mock(Gson.class);
-        this._fileReader = PowerMockito.mock(FileReader.class);
-    }
-    
-    @After
-    public void tearDown() {
+        MockitoAnnotations.initMocks(this);
+        this._selectedHost = "Github";
+        this._dirName = System.getProperty("user.home") + "\\.GitRepoViewer\\";
+        this._filePrefix = "User.json";
     }
 
     /**
      * Test of getUser method, of class UserRepository.
+     * @throws java.lang.Exception
      */
     @Test
+    @Ignore
     public void testGetUser() throws Exception {
         System.out.println("getUser");
         //        _userRepo.setSelectedHost(host);
-        
-        PowerMockito.whenNew(FileReader.class).withArguments("PathToFile.txt").thenReturn(this._fileReader);
-
         String expResult = "ChrisLE";
+        this._userRepo = new UserRepository(this._gsonProvider);
 
-        PowerMockito.when(this._gsonProvider.fromJson(this._fileReader, User.class)).thenReturn(new User("ChrisLE", "MyToken"));
-
-        this._userRepo = new UserRepository(_gsonProvider);
+        PowerMockito.when(this._gsonProvider.fromJson(new FileReader(this._dirName + this._selectedHost + this._filePrefix), User.class)).thenReturn(new User("ChrisLE", "MyToken"));
         User result = this._userRepo.getUser();
         
         assertEquals(expResult, result.getUserName());
+    }
+    
+    /**
+     * Test of getUser method, of class UserRepository.
+     * @throws java.lang.Exception
+     */
+    @Test(expected = FileNotFoundException.class)
+    public void testGetUserFileNotFound() throws Exception {
+        System.out.println("getUserFileNotFound");
+        //        _userRepo.setSelectedHost(host);
+        String expResult = "ChrisLE";
+        this._userRepo = new UserRepository(this._gsonProvider);
+
+        PowerMockito.when(this._gsonProvider.fromJson(new FileReader(this._dirName + this._selectedHost + this._filePrefix), User.class)).thenReturn(new User("ChrisLE", "MyToken"));
+        User result = this._userRepo.getUser();
     }
 
 //    /**
