cc24c7f6478e9796e77ee4e67accd0e111429686
Chris <chrl@spreadshirt.net>
2016-01-04 10:52:23 +0100

    Fixed test for the userRepo.

--- Repository viewer/test/unit/src/org/chrisle/netbeans/modules/gitrepoviewer/repositories/UserRepositoryTest.java
+++ Repository viewer/test/unit/src/org/chrisle/netbeans/modules/gitrepoviewer/repositories/UserRepositoryTest.java
@@ -2,9 +2,10 @@ package org.chrisle.netbeans.modules.gitrepoviewer.repositories;
 
 import com.google.gson.Gson;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.Reader;
 import org.chrisle.netbeans.modules.gitrepoviewer.beans.User;
-import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -14,6 +15,7 @@ import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import static org.junit.Assert.assertEquals;
 
 /**
  *
@@ -33,32 +35,32 @@ public class UserRepositoryTest {
     
     @Rule
     public TemporaryFolder testFolder = new TemporaryFolder();
+    
+    public Reader _fileReader;
 
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
 //        MockitoAnnotations.initMocks(this);
         this._selectedHost = "Github";
-//        this._dirName = System.getProperty("user.home") + "\\.GitRepoViewer\\";
         this._filePrefix = "User.json";
+        
+        File tempFile = testFolder.newFile(this._selectedHost + this._filePrefix);
+        
+        this._fileReader = new FileReader(testFolder.getRoot().getPath() + "\\" + tempFile.getName());
+        this._userRepo = new UserRepository(this._gsonProvider, this._fileReader);
+
     }
 
     /**
      * Test of getUser method, of class UserRepository.
-     * @throws java.lang.Exception
      */
-//    @Ignore
     @Test
-    public void testGetUser() throws Exception {
+    public void testGetUser() {
         System.out.println("getUser");
+        
         String expResult = "ChrisLE";
-        this._userRepo = new UserRepository(this._gsonProvider);
-        
-        File tempFile = testFolder.newFile(this._selectedHost + this._filePrefix);
-//        File tempFolder = testFolder.newFile("folder");
-        
-        System.out.println(testFolder.getRoot().getName());
 
-        Mockito.when(this._gsonProvider.fromJson(new FileReader(testFolder.getRoot().getPath() + "\\" + tempFile.getName()), User.class)).thenReturn(new User("ChrisLE", "MyToken"));
+        Mockito.when(this._gsonProvider.fromJson(_fileReader, User.class)).thenReturn(new User("ChrisLE", "MyToken"));
         User result = this._userRepo.getUser();
         
         assertEquals(expResult, result.getUserName());
@@ -66,17 +68,16 @@ public class UserRepositoryTest {
     
     /**
      * Test of getUser method, of class UserRepository.
-     * @throws java.lang.Exception
+     * @throws java.io.FileNotFoundException
      */
 //    @Ignore
-//    @Test(expected = FileNotFoundException.class)
-//    public void testGetUserFileNotFound() throws Exception {
-//        System.out.println("getUserFileNotFound");
-//        this._userRepo = new UserRepository(this._gsonProvider);
-//        
-//        Mockito.when(this._gsonProvider.fromJson(new FileReader(this._dirName + this._selectedHost + this._filePrefix), User.class)).thenReturn(new User("ChrisLE", "MyToken"));
-//        User result = this._userRepo.getUser();
-//    }
+    @Test(expected = FileNotFoundException.class)
+    public void testGetUserFileNotFound() throws FileNotFoundException {
+        System.out.println("getUserFileNotFound");
+        
+        Mockito.when(this._gsonProvider.fromJson(new FileReader("Foooooooooooooooo.json"), User.class)).thenReturn(new User("ChrisLE", "MyToken"));
+        User result = this._userRepo.getUser();
+    }
 
 //    /**
 //     * Test of saveUser method, of class UserRepository.
