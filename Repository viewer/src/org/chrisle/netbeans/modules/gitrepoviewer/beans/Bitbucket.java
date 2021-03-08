517805c0f3dd725734ae18f17b36a776c976ef67
Chris <christian.lenz@doksafe.de>
2021-03-08 22:22:18 +0100

    Created suite.

--- Repository viewer/src/org/chrisle/netbeans/modules/gitrepoviewer/beans/Bitbucket.java
+++
@@ -1,9 +0,0 @@
-package org.chrisle.netbeans.modules.gitrepoviewer.beans;
-
-public class Bitbucket extends Host {
-    public Bitbucket() {
-        super.setHostName("Bitbucket");
-        super.setIcon("org/chrisle/netbeans/modules/gitrepoviewer/resources/bitbucket.png");
-    }
-    
-}
--- Repository viewer/src/org/chrisle/netbeans/modules/gitrepoviewer/beans/Github.java
+++
@@ -1,8 +0,0 @@
-package org.chrisle.netbeans.modules.gitrepoviewer.beans;
-
-public class Github extends Host {
-    public Github() {
-        super.setHostName("Github");
-        super.setIcon("org/chrisle/netbeans/modules/gitrepoviewer/resources/github.png");
-    }
-}
